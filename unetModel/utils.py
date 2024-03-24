ac8155cb40edbe0ca4f498f3c980a8266802aa78
Elia <elia.weiss@gmail.com>
2024-03-24 18:40:42 +0545

    1

--- unetModel/utils.py
+++ unetModel/utils.py
@@ -2,7 +2,7 @@
 
 import torch
 import torchvision
-from dataset import CarvanaDataset
+from carvanaDataset import CarvanaDataset
 from torch.utils.data import DataLoader
 
 
@@ -54,3 +54,29 @@ def get_loaders(
         shuffle=False,
     )
     return train_loader, val_loader
+
+
+def check_accuracy(loader, model, device="cuda"):
+    num_correct = 0
+    num_pixels = 0
+    model.eval()
+    
+    with torch.no_grad():
+        for x,y in loader:
+            x = x.to(device)
+            y = y.to(device)
+            preds = torch.sigmoid(model(x))
+            preds = (preds>0.5).float()
+            num_correct += (preds == y).sum()
+            num_pixels += torch.numel(preds)
+            dice_score += (2*(preds*y).sum())/(
+                (preds+y).sum() + 1e-8
+            )
+            
+        print(
+            f"Got {num_correct}/{num_pixels} with acc {num_correct/num_pixels*100:.2f}"
+        )
+        
+        print(f"Dice score: {dice_score/len(loader)}")
+        
+        model.train()

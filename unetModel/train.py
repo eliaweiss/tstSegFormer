1a334aa6da91a843b53a848ee6ec99174bd400a0
Elia <elia.weiss@gmail.com>
2024-03-24 18:23:56 +0545

    train

--- unetModel/train.py
+++ unetModel/train.py
@@ -18,46 +18,79 @@ from utils import (
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 32
-NUM_EPOCHS = 3 #100
+NUM_EPOCHS = 3  # 100
 NUM_WORKERS = 2
-IMAGE_HEIGHT = 160 # 1280 original
-IMAGE_WIDTH = 240 # 1918 original
+IMAGE_HEIGHT = 160  # 1280 original
+IMAGE_WIDTH = 240  # 1918 original
 PIN_MEMORY = True
 LOAD_MODEL = False
 TRAIN_IMG_DIR = "data/train_images/"
 TRAIN_MASK_DIR = "data/train_masks/"
 VAL_IMG_DIR = "data/val_images/"
-VAL_MASK_DIR = "data/val_masks/"    
-    
+VAL_MASK_DIR = "data/val_masks/"
+
+
 def train_fn(loader, model, optimizer, loss_fn, scaler):
-    loop= tqdm(loader)
+    loop = tqdm(loader)
     for batch_idx, (data, targets) in enumerate(loop):
         data = data.to(device=DEVICE)
         targets = targets.float().unsqueeze(1).to(device=DEVICE)
-    
+
     # forward
     with torch.cuda.amp.autocast():
-        predictions =  model(data)
+        predictions = model(data)
         loss = loss_fn(predictions, targets)
-        
-        
+
     # backward
     optimizer.zero_grad()
     scaler.scale(loss).backward()
     scaler.step(optimizer)
     scaler.update()
-    
+
     # update tqdm
     loop.set_postfix(loss=loss.item())
-        
-        
-        
-    
-    
+
+
 def main():
-    pass
+    train_transform = A.Compose(
+        [
+            A. Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A. Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    
+    train_loaders, val_loaders = get_loaders(
+        TRAIN_IMG_DIR,
+        TRAIN_MASK_DIR,
+        VAL_IMG_DIR,
+        VAL_MASK_DIR,
+        BATCH_SIZE,
+        train_transform,
+        val_transforms
+    )
 
 if __name__ == "__main__":
     main()
-
-

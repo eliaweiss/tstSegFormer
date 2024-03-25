275f3c93f381e68803ea6da0a3cf110697ab83c6
Ubuntu <ubuntu@ip-172-31-12-107.eu-west-3.compute.internal>
2024-03-25 10:44:29 +0000

    split

---
+++ splitSegFormer.py
@@ -0,0 +1,59 @@
+import os
+import random
+from shutil import move
+import glob
+from os.path import join, isfile, splitext
+
+BASE_PATH = "/home/ubuntu/work/carvana/segFormer"
+
+train_path = f"{BASE_PATH}/train/"
+val_path = f"{BASE_PATH}/valid/"
+test_path = f"{BASE_PATH}/test/"
+
+train_images = os.listdir(train_path)
+train_images = list(filter(lambda x: "jpg" in x, train_images))
+
+split_size = int(0.05 * len(train_images))  # % of the data for validation
+
+################################
+val_images = random.sample(train_images, split_size)
+# Match masks with corresponding images
+val_masks = []
+for image in val_images:
+    # Extract mask base name
+    mask_base_name = image.split(".")[0]
+    # Loop through validation images to find match
+    val_masks.append(f"{mask_base_name}_mask.gif")
+    
+val_masks.sort()
+val_images.sort()
+
+for image, mask in zip(val_images, val_masks):
+    mask_base_name = image.split(".")[0]
+    assert mask_base_name in mask
+
+    move(os.path.join(train_path, image), os.path.join(val_path, image))
+    move(os.path.join(train_path, mask), os.path.join(val_path, mask))
+    # break
+
+################################
+
+test_images = random.sample(train_images, split_size)
+# Match masks with corresponding images
+test_masks = []
+for image in test_images:
+    # Extract mask base name
+    mask_base_name = image.split(".")[0]
+    # Loop through testidation images to find match
+    test_masks.append(f"{mask_base_name}_mask.gif")
+    
+test_masks.sort()
+test_images.sort()
+
+for image, mask in zip(test_images, test_masks):
+    mask_base_name = image.split(".")[0]
+    assert mask_base_name in mask
+
+    move(os.path.join(train_path, image), os.path.join(test_path, image))
+    move(os.path.join(train_path, mask), os.path.join(test_path, mask))
+    # break
